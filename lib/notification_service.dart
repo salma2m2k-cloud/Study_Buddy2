26859ea2b111f064db02c0eaab80705cc8d1f1a4
Salma <salma2m2k@gmail.com>
2026-09-06 20:13:17 +0300

    Add import for dart:typed_data in notification_service

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,3 +1,4 @@
+import 'dart:typed_data';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:timezone/timezone.dart' as tz;
